2e40f755b81fc7a37469b3630e87e98ac8c19d2c
Anton <78860436+lachesis17@users.noreply.github.com>
2024-01-09 19:59:42 +0000

    i hate javascript

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
 app.use(cors()); 
-app.use(body_parser.json());
+app.use(bodyParser.json());
 
 app.post('/count-words', (req, res) => {
   const {file} = req.body;
@@ -54,3 +50,7 @@ function count_words(file) { // kinda translated my python function here...... c
         'The most frequently occurring word length is': most_freq_lens_str,
     };
 }
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
