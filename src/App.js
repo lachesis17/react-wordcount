992c96f9144b99a3ac62d88790ff2bb981dfb0cb
Anton <antonleighf@gmail.com>
2024-01-10 10:46:07 +0000

    trial and error, trial and error, error, error

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 
   const handle_entry = async (text) => {
     try {
-      const response = await fetch('https://react-wordcount-kappa.vercel.app/count-words', {
+      const response = await fetch('https://react-wordcount-kappa.vercel.app/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
