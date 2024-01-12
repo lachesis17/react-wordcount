e1610f0bff0b639893951ee0de07902ad9017634
Anton <antonleighf@gmail.com>
2024-01-12 13:25:52 +0000

    add fade in animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 import TextInput from './TextInput';
 
@@ -27,15 +28,25 @@ function App() {
     }
   };
 
+  const [fade_header, setFadeInHeader] = useState(false); // states for rendering with a fade animation
+  const [fade_text, setFadeInTextInput] = useState(false);
+  const [fade_img, setFadeInImage] = useState(false);
+
+  useEffect(() => { // stagger the fades - unmountOnExit prop is needed in the transition to prevent initial rendering
+    setTimeout(() => {setFadeInHeader(true);}, 625);
+    setTimeout(() => {setFadeInTextInput(true);}, 1250);
+    setTimeout(() => {setFadeInImage(true);}, 1875);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='Headers'>Gimme some words. I will count them and stuff.</h1>
-        <TextInput className='Text_Submit' onSubmit={handle_entry} />
-        <br></br>
-        <a href="https://github.com/lachesis17/react-wordcount">
-        <img id="git" src="/gitLogo.png" alt="" width="200px" height="200px" />
-        </a>
+        <CSSTransition in={fade_header} timeout={1250} classNames="fade" unmountOnExit>
+          <h1 className="Headers">Gimme some words. I will count them and stuff.</h1></CSSTransition>
+        <CSSTransition in={fade_text} timeout={1250} classNames="fade" unmountOnExit>
+          <TextInput className="Text_Submit" onSubmit={handle_entry} /></CSSTransition>
+        <CSSTransition in={fade_img} timeout={1250} classNames="fade" unmountOnExit>
+          <a href="https://github.com/lachesis17/react-wordcount" className="git-link"><img id="git" src="/gitLogo.png" alt="" width="200px" height="200px" /></a></CSSTransition>
       </header>
     </div>
   );
